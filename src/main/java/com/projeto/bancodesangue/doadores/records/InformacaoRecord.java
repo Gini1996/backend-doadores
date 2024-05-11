aedbbcbdd3f0cd90533d8c3922cdbb8ee444bb14
Leonardo Ferreira <leonardo.gini.96@hotmail.com>
2024-05-11 02:35:02 -0300

    #004 - Implementacao Doador por Estado

---
+++ src/main/java/com/projeto/bancodesangue/doadores/records/InformacaoRecord.java
@@ -0,0 +1,6 @@
+package com.projeto.bancodesangue.doadores.records;
+
+public record InformacaoRecord(Long idInformacao, double altura, double peso, String tipoSanguineo, Long idDoador) 
+{
+	
+}
--- src/main/java/com/projeto/bancodesangue/doadores/repositories/DoadorRepository.java
+++ src/main/java/com/projeto/bancodesangue/doadores/repositories/DoadorRepository.java
@@ -1,10 +1,24 @@
 package com.projeto.bancodesangue.doadores.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.projeto.bancodesangue.doadores.entities.Doador;
+import com.projeto.bancodesangue.doadores.projections.DoadorPorEstadoProjection;
 
 public interface DoadorRepository extends JpaRepository<Doador, Long>
 {
-	
+	@Query(nativeQuery = true, value = """
+			SELECT COUNT(*) candidatos, e.estado
+		     FROM doador d 
+	       INNER JOIN endereco e ON e.id_endereco = d.id_endereco  
+	       INNER JOIN informacao_doador i ON i.id_doador = d.id_doador
+	       WHERE i.peso > 50 
+	       AND AGE(CURRENT_DATE, d.data_nascimento)>= INTERVAL '16 years'
+	       AND AGE(CURRENT_DATE, d.data_nascimento)<= INTERVAL '69 years'
+	       GROUP BY e.estado 
+	       ORDER BY e.estado""")
+	    List<DoadorPorEstadoProjection> countDoadorPorEstado();
 }
